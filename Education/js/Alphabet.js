93a9dfd64ead8b3934117c35c513eb799c8d9fe9
ketharin <ketharinn@gmail.com>
2023-08-28 23:13:07 +0600

    hello kids

---
+++ Education/js/Alphabet.js
@@ -0,0 +1,437 @@
+
+function openPopup_a() {
+    let popup = document.getElementById("popup_a");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("a")
+        audio.play();     
+ }
+ function closePopup_a() {
+    let popup = document.getElementById("popup_a");
+    popup.classList.remove("open-popup_alpha");
+}
+
+
+function openPopup_b() {
+    let popup = document.getElementById("popup_b");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("b")
+        audio.play();    
+ }
+ function closePopup_b() {
+    let popup = document.getElementById("popup_b");
+    popup.classList.remove("open-popup_alpha");
+}
+
+
+ function openPopup_c() {
+    let popup = document.getElementById("popup_c");
+    popup.classList.add("open-popup_alpha"); 
+    var audio = document.getElementById("c")
+        audio.play();   
+ }
+ function closePopup_c() {
+    let popup = document.getElementById("popup_c");
+    popup.classList.remove("open-popup_alpha");
+}
+
+
+ function openPopup_d() {
+    let popup = document.getElementById("popup_d");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("d")
+        audio.play();   
+ }
+ function closePopup_d() {
+    let popup = document.getElementById("popup_d");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_e() {
+    let popup = document.getElementById("popup_e");
+    popup.classList.add("open-popup_alpha");
+     var audio = document.getElementById("e")
+        audio.play();   
+ }
+ function closePopup_e() {
+    let popup = document.getElementById("popup_e");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_f() {
+    let popup = document.getElementById("popup_f");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("f")
+        audio.play();   
+ }
+ function closePopup_f() {
+    let popup = document.getElementById("popup_f");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_g() {
+    let popup = document.getElementById("popup_g");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("g")
+        audio.play();   
+ }
+ function closePopup_g() {
+    let popup = document.getElementById("popup_g");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_h() {
+    let popup = document.getElementById("popup_h");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("h")
+        audio.play();   
+ }
+ function closePopup_h() {
+    let popup = document.getElementById("popup_h");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_i() {
+    let popup = document.getElementById("popup_i");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("i")
+        audio.play();
+   
+ }
+ function closePopup_i() {
+    let popup = document.getElementById("popup_i");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+function openPopup_j() {
+    let popup = document.getElementById("popup_j");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("j")
+        audio.play();   
+ }
+ function closePopup_j() {
+    let popup = document.getElementById("popup_j");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_k() {
+    let popup = document.getElementById("popup_k");
+    popup.classList.add("open-popup_alpha"); 
+    var audio = document.getElementById("k")
+        audio.play();  
+ }
+ function closePopup_k() {
+    let popup = document.getElementById("popup_k");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_l() {
+    let popup = document.getElementById("popup_l");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("l")
+        audio.play();   
+ }
+ function closePopup_l() {
+    let popup = document.getElementById("popup_l");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_m() {
+    let popup = document.getElementById("popup_m");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("m")
+        audio.play();   
+ }
+ function closePopup_m() {
+    let popup = document.getElementById("popup_m");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_n() {
+    let popup = document.getElementById("popup_n");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("n")
+        audio.play();   
+ }
+ function closePopup_n() {
+    let popup = document.getElementById("popup_n");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_o() {
+    let popup = document.getElementById("popup_o");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("o")
+        audio.play();   
+ }
+ function closePopup_o() {
+    let popup = document.getElementById("popup_o");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_p() {
+    let popup = document.getElementById("popup_p");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("p")
+        audio.play();   
+ }
+ function closePopup_p() {
+    let popup = document.getElementById("popup_p");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+function openPopup_q() {
+    let popup = document.getElementById("popup_q");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("q")
+        audio.play();   
+ }
+ function closePopup_q() {
+    let popup = document.getElementById("popup_q");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_r() {
+    let popup = document.getElementById("popup_r");
+    popup.classList.add("open-popup_alpha"); 
+    var audio = document.getElementById("r")
+        audio.play();  
+ }
+ function closePopup_r() {
+    let popup = document.getElementById("popup_r");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_s() {
+    let popup = document.getElementById("popup_s");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("s")
+        audio.play(); 
+
+ }
+ function closePopup_s() {
+    let popup = document.getElementById("popup_s");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_t() {
+    let popup = document.getElementById("popup_t");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("t")
+        audio.play();   
+ }
+ function closePopup_t() {
+    let popup = document.getElementById("popup_t");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_u() {
+    let popup = document.getElementById("popup_u");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("u")
+        audio.play();   
+ }
+ function closePopup_u() {
+    let popup = document.getElementById("popup_u");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_v() {
+    let popup = document.getElementById("popup_v");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("v")
+        audio.play();   
+ }
+ function closePopup_v() {
+    let popup = document.getElementById("popup_v");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_w() {
+    let popup = document.getElementById("popup_w");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("w")
+        audio.play();   
+ }
+ function closePopup_w() {
+    let popup = document.getElementById("popup_w");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_x() {
+    let popup = document.getElementById("popup_x");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("x")
+        audio.play();   
+ }
+ function closePopup_x() {
+    let popup = document.getElementById("popup_x");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_y() {
+    let popup = document.getElementById("popup_y");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("y")
+        audio.play();   
+ }
+ function closePopup_y() {
+    let popup = document.getElementById("popup_y");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+
+ function openPopup_z() {
+    let popup = document.getElementById("popup_z");
+    popup.classList.add("open-popup_alpha");
+    var audio = document.getElementById("z")
+        audio.play();   
+ }
+ function closePopup_z() {
+    let popup = document.getElementById("popup_z");
+    popup.classList.remove("open-popup_alpha");
+     
+ }
+/*------------------------music-----------------------------------*/
+function play_a(){
+       
+        var audio = document.getElementById("a")
+        audio.play();
+    }
+     function play_b(){
+        var audio = document.getElementById("b")
+        audio.play();
+    }
+       function play_c(){
+        var audio = document.getElementById("c")
+        audio.play();
+    }
+       function play_d(){
+        var audio = document.getElementById("d")
+        audio.play();
+    }
+    function play_e(){
+        var audio = document.getElementById("e")
+        audio.play();
+    }
+     function play_f(){
+        var audio = document.getElementById("f")
+        audio.play();
+    }
+       function play_g(){
+        var audio = document.getElementById("g")
+        audio.play();
+    }
+       function play_h(){
+        var audio = document.getElementById("h")
+        audio.play();
+    }
+
+    function play_i(){
+        var audio = document.getElementById("i")
+        audio.play();
+    }
+     function play_j(){
+        var audio = document.getElementById("j")
+        audio.play();
+    }
+       function play_k(){
+        var audio = document.getElementById("k")
+        audio.play();
+    }
+       function play_l(){
+        var audio = document.getElementById("l")
+        audio.play();
+    }
+    function play_m(){
+        var audio = document.getElementById("m")
+        audio.play();
+    }
+     function play_n(){
+        var audio = document.getElementById("n")
+        audio.play();
+    }
+       function play_o(){
+        var audio = document.getElementById("o")
+        audio.play();
+    }
+       function play_p(){
+        var audio = document.getElementById("p")
+        audio.play();
+    }
+     function play_q(){
+        var audio = document.getElementById("q")
+        audio.play();
+    }
+     function play_r(){
+        var audio = document.getElementById("r")
+        audio.play();
+    }
+       function play_s(){
+        var audio = document.getElementById("s")
+        audio.play();
+    }
+       function play_t(){
+        var audio = document.getElementById("t")
+        audio.play();
+    }
+    function play_u(){
+        var audio = document.getElementById("u")
+        audio.play();
+    }
+     function play_v(){
+        var audio = document.getElementById("v")
+        audio.play();
+    }
+       function play_w(){
+        var audio = document.getElementById("w")
+        audio.play();
+    }
+       function play_x(){
+        var audio = document.getElementById("x")
+        audio.play();
+    }
+
+    function play_y(){
+        var audio = document.getElementById("y")
+        audio.play();
+    }
+     function play_z(){
+        var audio = document.getElementById("z")
+        audio.play();
+    }
+
+    /*----------------------------------vowel-------------------------------*/
+    document.querySelector('body')
+    .addEventListener('mouseover',eyeball);
+    function eyeball(){
+      var eye = document.querySelectorAll('.eye');
+      eye.forEach(function(eye){
+         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth/2);
+         let y = (eye.getBoundingClientRect().top) + (eye.clientHeight/2);
+         let radian = Math.atan2(event.pageX - x, event.pageY - y);
+         let rot =(radian * (170/ Math.PI) * -1)+270;
+         eye.style.transform = "rotate("+rot +"deg)";
+      })
+    }
+
